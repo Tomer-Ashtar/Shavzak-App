851836bbf32f7eb28b0d6314443b3cb97fc5787a
Tomer-Ashtar <tomerashtar82@gmail.com>
2025-11-05 16:56:55 +0200

    extra col

--- workers/models.py
+++ workers/models.py
@@ -9,8 +9,17 @@ class Worker(models.Model):
         ('soldier', 'חייל'),
     ]
     
+    DEPARTMENT_CHOICES = [
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('other', 'אחר'),
+    ]
+    
     name = models.CharField(max_length=200)
     title = models.CharField(max_length=50, choices=TITLE_CHOICES)
+    department = models.CharField(max_length=20, choices=DEPARTMENT_CHOICES, blank=True, null=True, verbose_name='מחלקה')
     hard_chores_counter = models.IntegerField(default=0)
     outer_partner_counter = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
